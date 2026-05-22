c8d4673330bc2edb1f1f4faef4ab2c257a02b2aa
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-05-22 23:19:41 +0300

    feat: add user login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -36,8 +36,63 @@ const register = async (req, res) => {
         console.log ('');
         res.status(500).json({ error: 'Помилка на сервері під час реєстрації'});
     }
-}
+};
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({ error: 'Email та пароль є обов’язковими'});
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {email},
+        });
+
+        if(!user) {
+            return res.status(401).json({ error: 'Неправильний email або пароль'});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            return res.status(401).json({ error: 'Неправильний email або пароль'});
+        }
+
+        const token = jwt.sign({ userId: user.id}, JWT_SECRET, { expiresIn: '24h'});
+
+        res.json({
+            message: 'Вхід успішний',
+            token,
+            user: { id: user.id, email: user.email, name: user.name},
+        });
+    }
+    catch (error) {
+        console.error('Помилка входу:', error);
+        res.status(500).json({ error: 'Помилка на сервері під час входу'});
+    }
+};
+
+const getProfile = async (req, res) => {
+    try{
+        const user = await prisma.user.findUnique({
+            where: { id: req.user.userId },
+            select: { id: true, email: true, name: true, createdAt: true },
+        });
+
+    if (!user) {
+      return res.status(404).json({ error: 'Користувача не знайдено' });
+    }
+    res.json(user);
+    } 
+    catch (error) {
+    console.error('Помилка отримання профілю:', error);
+    res.status(500).json({ error: 'Помилка сервера' });  
+    }
+};
 
 module.exports = {
-  register
+  register,
+  login,
+  getProfile,
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { register } = require('../controllers/authController');
+const { login } = require('../controllers/authController')
+const { getProfile } = require('../controllers/authController')
 
 router.post('/register', register);
+router.post('/login', login);
+router.get('/getProfile', getProfile);
 
 module.exports = router;

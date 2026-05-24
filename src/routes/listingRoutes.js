94ec94fdc6494ae954bcf1b7eb0c5422f6262dde
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-05-24 23:05:45 +0300

    feat: add Cloudinary image upload for Listings

--- src/routes/listingRoutes.js
+++ src/routes/listingRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getListings, createListing } from '../controllers/listingController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
+import upload from '../middlewares/uploadMiddleware.js';
 
 const router = express.Router();
 
@@ -8,6 +9,6 @@ const router = express.Router();
 router.get('/', getListings);
 
 // POST /api/listings - створити оголошення (потрібен токен авторизації)
-router.post('/', authMiddleware, createListing);
+router.post('/', authMiddleware, upload.single('image'), createListing);
 
 export default router;

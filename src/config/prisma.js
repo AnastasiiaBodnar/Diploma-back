3ee5ee3fc8291787470a23988ea14b2a85cc2642
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2026-05-28 12:45:53 +0300

    feat: add review model to prisma schema

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -4,15 +4,12 @@ import { PrismaClient } from '@prisma/client';
 
 const { Pool } = pg;
 
-// Створюємо пул підключень до PostgreSQL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-// Ініціалізуємо Prisma адаптер
 const adapter = new PrismaPg(pool);
 
-// Створюємо екземпляр PrismaClient з адаптером
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
